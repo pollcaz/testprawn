ff7eacf1c46f5d43a066412661b9ef577f1e8d9c
Paulo Carmona <pcarmona@misena.edu.co>
2013-06-05 10:51:14 -0500

    Inicio Aplicacion con Gema Prawn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testprawn::Application.config.secret_token = 'ae280ce74d18fc10c56a53925b9f9938aa6bf05d2414ff3105c884338411cb0c452b6590dc9450358e9a7aac259cb7347f7de4f4e67e94727a9b055b3a0e6ce6'
